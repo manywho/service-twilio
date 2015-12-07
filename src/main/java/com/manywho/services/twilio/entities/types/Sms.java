d9e53e237b076736c69c373b19b0278f4c107a98
Jonjo McKay <jonjo.mckay@manywho.com>
2015-12-07 12:17:49 +0000

    Added validation annotations to the Sms type entity

--- src/main/java/com/manywho/services/twilio/entities/types/Sms.java
+++ src/main/java/com/manywho/services/twilio/entities/types/Sms.java
@@ -2,16 +2,20 @@ package com.manywho.services.twilio.entities.types;
 
 import com.manywho.sdk.services.annotations.Property;
 import com.manywho.sdk.services.annotations.Type;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Type(com.manywho.services.twilio.types.Sms.NAME)
 public class Sms {
 
+    @NotEmpty(message = "The From value must not be null or blank")
     @Property("From")
     private String from;
 
+    @NotEmpty(message = "The To value must not be null or blank")
     @Property("To")
     private String to;
 
+    @NotEmpty(message = "The Body value must not be null or blank")
     @Property("Body")
     private String body;
 
